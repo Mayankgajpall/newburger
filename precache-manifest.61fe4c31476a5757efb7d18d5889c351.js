93096785ccef677a67cbbb5e08ec31579294d23a
unknown <mayankgajpal.11@gmail.com>
2020-08-25 00:46:32 +0530

    Updates

---
+++ precache-manifest.61fe4c31476a5757efb7d18d5889c351.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e46d225fc4ed34a8429e06ff2a4178dc",
+    "url": "/newburger/index.html"
+  },
+  {
+    "revision": "459aee6896a6e99b2dae",
+    "url": "/newburger/static/css/main.0759e08a.chunk.css"
+  },
+  {
+    "revision": "b706db04562c6e40e37a",
+    "url": "/newburger/static/js/2.d8e4de39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/newburger/static/js/2.d8e4de39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "459aee6896a6e99b2dae",
+    "url": "/newburger/static/js/main.5d0de614.chunk.js"
+  },
+  {
+    "revision": "37936d6d7537e98f0dc5",
+    "url": "/newburger/static/js/runtime-main.64bf694d.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/newburger/static/media/burger-logo.b8503d26.png"
+  }
+]);
